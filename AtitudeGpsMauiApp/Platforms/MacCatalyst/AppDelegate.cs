9aeb885c792bc60cf250dbac720dc99010507a83
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-11 19:57:52 -0300

    Add project files.

---
+++ AtitudeGpsMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AtitudeGpsMauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
