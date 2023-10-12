a7f72fccd75eba0b3f0266d84eef1f5ad2882dde
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-12 03:39:17 -0300

    Implementado o comportamento de layout na tela principal.

--- AtitudeGpsMauiApp/Pages/MainPage.xaml.cs
+++ AtitudeGpsMauiApp/Pages/MainPage.xaml.cs
@@ -6,5 +6,41 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    void AtivaLed(object sender)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            this.btnMonitorar.IsEnabled = false;
+            this.btnParar.IsEnabled = true;
+            this.btnEnviar.IsEnabled = false;
+            this.btnLimparLog.IsEnabled = false;
+            this.lblStatus.Background = Colors.Green;
+            this.lblStatus.Text = "ONLINE";
+        });
+    }
+
+    void DesativaLed(object sender)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            this.btnMonitorar.IsEnabled = true;
+            this.btnParar.IsEnabled = false;
+            this.btnEnviar.IsEnabled = true;
+            this.btnLimparLog.IsEnabled = true;
+            this.lblStatus.Background = Colors.Red;
+            this.lblStatus.Text = "OFFLINE";
+        });
+    }
+
+    private void btnMonitorar_Clicked(object sender, EventArgs e)
+    {
+        AtivaLed(sender);
+    }
+
+    private void btnParar_Clicked(object sender, EventArgs e)
+    {
+        DesativaLed(sender);
+    }
 }
 
