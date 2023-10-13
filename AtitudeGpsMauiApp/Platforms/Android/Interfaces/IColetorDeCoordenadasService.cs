3322ad152ea00b40162fc4dc578b00075d0296a3
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-13 03:21:50 -0300

    Criada a classe do serviço de coleta de coordenadas do dispositivo.

---
+++ AtitudeGpsMauiApp/Platforms/Android/Interfaces/IColetorDeCoordenadasService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Platforms.Android.Interfaces
+{
+    public interface IColetorDeCoordenadasService
+    {
+        void IniciaServico();
+        void EncerraServico();
+        bool IsServicoEmExecucao();
+    }
+}
