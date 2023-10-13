ab84294a8c46e2fd0b498582c47feb24557c5424
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-13 03:19:15 -0300

    Criado o serviço de exibição de caixa de mensagens na tela.

---
+++ AtitudeGpsMauiApp/Services/Implementations/MessageBoxService.cs
@@ -0,0 +1,17 @@
+﻿using AtitudeGpsMauiApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Services.Implementations
+{
+    public class MessageBoxService : IMessageBoxService
+    {
+        public async Task ShowAsync(string mensagem)
+        {
+            await Application.Current.MainPage.DisplayAlert("Atitude Gps", mensagem, "OK");
+        }
+    }
+}
