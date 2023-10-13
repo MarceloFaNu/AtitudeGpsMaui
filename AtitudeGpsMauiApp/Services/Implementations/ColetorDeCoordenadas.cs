db0b0f43919238457100d0cf34a34eb7b390d270
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-13 03:20:06 -0300

    Criada a classe que realiza a tarefa principal do serviço de obtenção das coordenadas do dispositivo.

---
+++ AtitudeGpsMauiApp/Services/Implementations/ColetorDeCoordenadas.cs
@@ -0,0 +1,92 @@
+﻿using AtitudeGpsMauiApp.Core.Models;
+using AtitudeGpsMauiApp.Services.Interfaces;
+using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Services.Implementations
+{
+    public class ColetorDeCoordenadas : IColetorDeCoordenadas
+    {
+        private readonly IMessageBoxService _msgBox;
+
+        public ColetorDeCoordenadas(IMessageBoxService msgBox)
+        {
+            _msgBox = msgBox;
+        }
+
+        public bool InicializaColetor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ColetaGps()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReiniciaContadorDeTicks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LimpaCoordenadas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FinalizaColetor()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool ExecuteEmTryCatch(Action action)
+        {
+            if (action == null) return false;
+            string msgDeErro = string.Empty;
+
+            try
+            {
+                action.Invoke();
+                return true;
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                msgDeErro = "Este dispositivo não possui a funcionalidade de GPS.";
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                msgDeErro = "Ative o serviço de GPS do dispositivo.";
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                msgDeErro = "Conceda permissão para o aplicativo acessar as informações de GPS do dispositivo. Sua privacidade sempre será respeitada.";
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                msgDeErro = ex.Message;
+            }
+
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await _msgBox.ShowAsync(msgDeErro);
+            });
+
+            return false;
+        }
+
+        private void LogaGps(Snapshot snapshot)
+        {
+            string locAtualJsonObject = JsonSerializer.Serialize(snapshot);
+            System.Diagnostics.Debug.WriteLine(snapshot);
+            //File.AppendAllText(DashboardPage.CoordsLog, locAtualJsonObject + "\n");
+        }
+    }
+}
---
+++ AtitudeGpsMauiApp/Services/Interfaces/IColetorDeCoordenadas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Services.Interfaces
+{
+    public interface IColetorDeCoordenadas
+    {
+        bool InicializaColetor();
+        bool ColetaGps();
+        void ReiniciaContadorDeTicks();
+        void LimpaCoordenadas();
+        void FinalizaColetor();
+    }
+}
