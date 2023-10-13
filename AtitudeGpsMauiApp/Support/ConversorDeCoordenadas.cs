120996b6a9389e0a96bfce496afdb984b53b3624
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-13 03:16:25 -0300

    Criada uma classe utilitária para manuseio dos valores das coordenadas do dispositivo.

---
+++ AtitudeGpsMauiApp/Support/ConversorDeCoordenadas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Support
+{
+    public class ConversorDeCoordenadas
+    {
+        public Location AplicaLimiteDeDigitosComArredondamento(Location loc, int qtdDigitos)
+        {
+            loc.Latitude = Math.Round(loc.Latitude, qtdDigitos);
+            loc.Longitude = Math.Round(loc.Longitude, qtdDigitos);
+            return loc;
+        }
+
+        public Location AplicaLimiteDeDigitosSemArredondamento(Location loc, int qtdDigitos)
+        {
+            loc.Latitude = Math.Truncate(loc.Latitude * qtdDigitos) / qtdDigitos;
+            loc.Longitude = Math.Truncate(loc.Latitude * qtdDigitos) / qtdDigitos;
+            return loc;
+        }
+    }
+}
