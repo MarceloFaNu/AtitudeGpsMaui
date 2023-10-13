e90107226e2022a3ce7e816d841240874c86a275
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-10-13 03:22:25 -0300

    Criada a classe de injeção de dependências.

---
+++ AtitudeGpsMauiApp/Configuration/DependencyInjection.cs
@@ -0,0 +1,53 @@
+﻿using AtitudeGpsMauiApp.Domain.Constantes;
+using AtitudeGpsMauiApp.Infrastructure.Implementations;
+using AtitudeGpsMauiApp.Infrastructure.Interfaces;
+using AtitudeGpsMauiApp.Services.Implementations;
+using AtitudeGpsMauiApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureApplication(this MauiAppBuilder mauiBuilder)
+        {
+            RegisterServices(mauiBuilder);
+            ConfigureAppProperties();
+        }
+
+        private static void RegisterServices(MauiAppBuilder mauiBuilder)
+        {
+            mauiBuilder.Services.AddSingleton<IMessageBoxService, MessageBoxService>();
+            mauiBuilder.Services.AddSingleton<ILeitorDeCoordenadas, LeitorDeCoordenadas>();
+            mauiBuilder.Services.AddSingleton<IColetorDeCoordenadas, ColetorDeCoordenadas>();
+        }
+
+        private static void ConfigureAppProperties()
+        {
+            if (!Preferences.ContainsKey(PropriedadesDaAplicacao.PROP_INTERVALO_MINIMO))
+                Preferences.Set(PropriedadesDaAplicacao.PROP_INTERVALO_MINIMO, 5);
+
+            if (!Preferences.ContainsKey(PropriedadesDaAplicacao.PROP_FATOR_CASAS_DECIMAIS))
+                Preferences.Set(PropriedadesDaAplicacao.PROP_FATOR_CASAS_DECIMAIS, 5);
+
+            if (!Preferences.ContainsKey(PropriedadesDaAplicacao.PROP_DISTANCIA_MINIMA_VALIDA))
+                Preferences.Set(PropriedadesDaAplicacao.PROP_DISTANCIA_MINIMA_VALIDA, 10.0D);
+
+            if (!Preferences.ContainsKey(PropriedadesDaAplicacao.PROP_PRECISAO_DE_LOCALIZACAO))
+                Preferences.Set(PropriedadesDaAplicacao.PROP_PRECISAO_DE_LOCALIZACAO, (int)GeolocationAccuracy.Medium);
+
+            if (!Preferences.ContainsKey(PropriedadesDaAplicacao.PROP_GEOLOCATION_REQUEST_TIMEOUT))
+                Preferences.Set(PropriedadesDaAplicacao.PROP_GEOLOCATION_REQUEST_TIMEOUT, 5);
+
+            PropriedadesDaAplicacao.IntervaloMinimo = Preferences.Get(PropriedadesDaAplicacao.PROP_INTERVALO_MINIMO, 5);
+            PropriedadesDaAplicacao.FatorDeCasasDecimais = Preferences.Get(PropriedadesDaAplicacao.PROP_FATOR_CASAS_DECIMAIS, 5);
+            PropriedadesDaAplicacao.DistanciaMinimaValida = Preferences.Get(PropriedadesDaAplicacao.PROP_DISTANCIA_MINIMA_VALIDA, 10.0D);
+            PropriedadesDaAplicacao.PrecisaoDeGeolocalizacao = (GeolocationAccuracy)Preferences.Get(PropriedadesDaAplicacao.PROP_PRECISAO_DE_LOCALIZACAO, 3);
+            PropriedadesDaAplicacao.GeolocationRequestTimeout = Preferences.Get(PropriedadesDaAplicacao.PROP_GEOLOCATION_REQUEST_TIMEOUT, 5);
+        }
+    }
+}
