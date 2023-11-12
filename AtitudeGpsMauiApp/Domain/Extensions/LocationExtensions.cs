e3ff08ff87b267664f4cec7d434b800b829eb6f3
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-11-12 00:41:17 -0300

    Criado método de extensão para arrays de Location em que os elementos são deslocados um índice a menos, a fim de abrir um espaço no último índice para a inserção de uma nova Location.

---
+++ AtitudeGpsMauiApp/Domain/Extensions/LocationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Domain.Extensions
+{
+    public static class LocationExtensions
+    {
+        public static void ShiftUpLocationArrayItems(this Location[] items)
+        {
+            if (items.Length < 2) return;
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                items[i - 1] = items[i];
+            }
+
+            items[items.Length - 1] = null;
+        }
+    }
+}
