4fedc485013c724befc8f5dfd57efb69ee0b41e7
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-11-13 02:53:08 -0300

    Criados dois conversores de classes para usar nos bidings de frontend.

---
+++ AtitudeGpsMauiApp/Domain/Extensions/BooleanNegateConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Domain.Extensions
+{
+    public class BooleanNegateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolean = (bool)value;
+            return !boolean;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.Convert(value, targetType, parameter, culture);
+        }
+    }
+}
---
+++ AtitudeGpsMauiApp/Domain/Extensions/GeolocationAccuracyToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtitudeGpsMauiApp.Domain.Extensions
+{
+    public class GeolocationAccuracyToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GeolocationAccuracy geolocAcc = GeolocationAccuracy.Default;
+
+            if (value != null)
+                geolocAcc = (GeolocationAccuracy)value;
+
+            return geolocAcc.ToAccuracyString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string geolocString = (string)value;
+            return geolocString.ToGeolocationAccuracy();
+        }
+    }
+}
